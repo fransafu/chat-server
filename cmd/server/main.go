67390c89b0ba48108e62f12a8b41bdd056706e1b
Francisco Sanchez <fransafu@gmail.com>
2022-10-04 04:00:36 -0300

    feat: Add server startup

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/fransafu/chat-server/html"
+	"github.com/fransafu/chat-server/internal/chat_server"
 )
 
 func main() {
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+	hub := chat_server.NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		html.Chat(w, html.ChatParams{
-			Title: "Chat",
+			Title: "Chat Server",
 		})
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		chat_server.ServeWs(hub, w, r)
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
